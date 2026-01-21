9c1db4efbeb4e2a3e2c09ec402e2decce25d879e
Rafael Gessele <rafael.gessele@gmail.com>
2026-01-20 23:04:37 -0300

    feat(tests): update login page test to verify visibility of password field and login button

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -6,7 +6,9 @@ test("login page renders", async ({ page }) => {
   await expect(page).toHaveTitle(/Inaá Studio/i);
   await expect(page.getByAltText("Inaá Studio")).toBeVisible();
   await expect(page.getByLabel("Email")).toBeVisible();
+  await expect(page.getByLabel("Senha")).toBeVisible();
+  await expect(page.getByRole("button", { name: "Entrar" })).toBeVisible();
   await expect(
-    page.getByRole("button", { name: "Enviar Link de Acesso" })
+    page.getByRole("link", { name: "Esqueci minha senha" })
   ).toBeVisible();
 });
